74f3b2aa1a7a2c0f2355cf9ea4decbcb7da22843
Andrew Azores <me@andrewazor.es>
2025-12-04 15:55:54 -0500

    don't cache source builds

--- test.bash
+++ test.bash
@@ -5,7 +5,7 @@ podman rmi mystorage:latest
 if [ -z "${SOURCE_BUILD}" ]; then
   docker build --build-arg SEAWEED_VERSION="${1:-dev}" -t mystorage:latest storage -f storage/Dockerfile
 else
-  docker build --build-arg SEAWEED_VERSION="${1:-master}" -t mystorage:latest storage -f storage/Dockerfile.source
+  docker build --no-cache --build-arg SEAWEED_VERSION="${1:-master}" -t mystorage:latest storage -f storage/Dockerfile.source
 fi
 
 ./mvnw clean test -Dmode="${MODE:-tag}"
