58f4877b0bdfbe37eb1cc1bc4f66a01e6e2f4788
Andrew Azores <me@andrewazor.es>
2025-12-04 18:39:05 -0500

    fixup! SKIP_BUILD

--- test.bash
+++ test.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-podman rmi mystorage:latest
 
 if [ -z "${SKIP_BUILD}" ]; then
+    podman rmi mystorage:latest
     if [ -z "${SOURCE_BUILD}" ]; then
       docker build --build-arg SEAWEED_VERSION="${1:-dev}" -t mystorage:latest storage -f storage/Dockerfile
     else
