727f4ce6fa0c7af5a45e951597f4c0dc7a972793
Andrew Azores <me@andrewazor.es>
2025-12-04 18:29:46 -0500

    SKIP_BUILD

--- test.bash
+++ test.bash
@@ -2,10 +2,12 @@
 
 podman rmi mystorage:latest
 
-if [ -z "${SOURCE_BUILD}" ]; then
-  docker build --build-arg SEAWEED_VERSION="${1:-dev}" -t mystorage:latest storage -f storage/Dockerfile
-else
-  docker build --no-cache --build-arg SEAWEED_VERSION="${1:-master}" -t mystorage:latest storage -f storage/Dockerfile.source
+if [ -z "${SKIP_BUILD}" ]; then
+    if [ -z "${SOURCE_BUILD}" ]; then
+      docker build --build-arg SEAWEED_VERSION="${1:-dev}" -t mystorage:latest storage -f storage/Dockerfile
+    else
+      docker build --no-cache --build-arg SEAWEED_VERSION="${1:-master}" -t mystorage:latest storage -f storage/Dockerfile.source
+    fi
 fi
 
 ./mvnw clean test -Dmode="${MODE:-tag}"
